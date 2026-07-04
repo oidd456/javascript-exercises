6beb6f25bb90613fc8b56214aae2406599f55e5c
oidd456 <133511470+oidd456@users.noreply.github.com>
2026-07-04 21:35:02 +1000

    Completed 06 exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word, rep) {
+    let exit = word;
+    if (typeof rep === 'number' && rep >= 1) {
+        for (let i = 1; i < rep; i++) {
+            exit += word;
+        }
+        return exit;
+    } else if (typeof rep === 'number' && rep < 0){
+        return 'ERROR';
+    }
+    else {
+        return ''
+    }
 };
 
 // Do not edit below this line
