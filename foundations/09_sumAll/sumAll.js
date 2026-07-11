83a3338eafa8406c830fcbf80f87601f7d382c6d
oidd456 <133511470+oidd456@users.noreply.github.com>
2026-07-11 19:46:21 +1000

    Updated 09 commits

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(upper, lower) {
+    if (upper > 0 && lower > 0 && Number.isInteger(upper)
+        && Number.isInteger(lower)
+    ) {
+        if (upper > lower) {
+            let result = lower;
+            for (let i = lower + 1; i < upper + 1; i++) {
+                result += i;
+            }
+            return result;
+        } else if (upper < lower) {
+            let result = upper;
+            for (let i = upper + 1; i < lower + 1; i++) {
+                result += i;
+            }
+            return result;
+        } else {
+            return upper;
+        }
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
